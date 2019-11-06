5267afe3a758925d006974dfedb4b777a8e49c23
etahirovic1 <etahirovic1@etf.unsa.ba>
2019-11-06 14:40:30 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Fakultet.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr;
+
+public class Fakultet {
+    static int[] brojObaveznih = new int[6];
+    for(int i=0, i < 6; i++) {
+        brojObaveznih[i] = 6-i;
+    }
+
+}
---
+++ src/ba/unsa/etf/rpr/Semestar.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr;
+
+public class Semestar extends Fakultet {
+    int brojObaveznih = 0;
+
+    int getBrojObaveznih() {
+        return brojObaveznih;
+    }
+
+    
+
+}
